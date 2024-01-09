3486a3f1572a92144b87f655a761735796e6d357
Marko <markosusa567@gmail.com>
2024-01-10 00:24:01 +0100

    add dateToTime helper function

---
+++ src/util/dateToTime.js
@@ -0,0 +1,12 @@
+const dateToTime = (createdAt) => {
+  const date = new Date(createdAt);
+  return date.toLocaleString("en-GB", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+}
+
+export default dateToTime;
